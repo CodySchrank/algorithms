4cb9954c4debeec4a48f7209f7ed97856a49e47a
Cody Schrank <cody.schrank@gmail.com>
2016-11-14 14:30:01 -0800

    adding tests

--- LinkedList/test.js
+++ LinkedList/test.js
@@ -11,12 +11,15 @@ var expect = require("chai").expect;
 
 describe("Linked List",function(){
 	describe("#init",function(){
-		var list = new LinkedList("first node");
+		var data = "first node";
+		var list = new LinkedList(data);
+
 		it("should exist",function(){
-			return expect(list).to.be.a('function')
-		})
+			return expect(list).to.be.a('Object');
+		});
+		
 		it("should have a node field with a data attribute",function(){
-			return expect(list).to.have.property('node')
-		})
+			return expect(list).to.have.property('head').to.have.property('data').to.equal(data);
+		});
 	});
 });
